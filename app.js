f6dbff993265f9e7bd65b36c9c5bd10410f4be24
Inno Sufiyan <innosufiyan@gmail.com>
2021-04-01 08:08:37 -0700

    work complete

---
+++ app.js
@@ -0,0 +1,367 @@
+// Question1
+
+// alert("Good morning Pakistan");
+
+// Question2
+
+// alert("Error! Please Enter a Valid Password");
+
+// Question3
+
+// alert("Welcome to JS Land... \nHappy Coding!");
+
+// Question4
+
+// alert("Welcome to JS Land...");
+// alert("Happy Coding!");
+
+// Question5
+
+// console.log(alert("Welcome to JS Land..."));
+
+// Question6
+
+// Done
+
+// Question7
+
+// Done
+
+// Chapter2
+
+// Question1
+
+// var username;
+
+// Question2
+
+// var myName = "Muhammad Sufiyan";
+
+// Question3
+
+// var message;
+// message = "Hello World"
+// alert(message);
+
+// Question4
+
+// var studentName = "Jhone Doe";
+// var studentAge = "15 years old";
+// var studentSkills = "Certification Mobile Application Developer";
+
+
+// Question5
+
+// var awaeeVariable = "Pizza\npizz\npiz\npi\np";
+// alert(awaeeVariable);
+
+// Question6
+
+// var email = "innosufiyan@gmail.com";
+// alert("My email address is " + email);
+
+// Question7
+
+// var book = "A Smarter Way to Learn JavaScript";
+// alert("I am trying to learn from the book " + book)
+
+// Question8
+
+// document.write("Yah! I can write HTML content through JavaScript");
+
+// Question9
+
+// var motive = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert(motive);
+
+
+// Chapter3
+
+// Question1
+
+// var age = 30;
+// alert(age);
+
+// Question2
+
+// var webVisits = 9
+// alert("You have visited this website " + webVisits + " times");
+
+// Question3
+
+// var birthYear = 1991;
+// document.write("My birth year is " + birthYear + "< br >")
+
+// document.write("Data type of my declared variable is ");
+
+
+
+// Question4
+
+// var visitorsName = prompt("visitors Name");
+// var productTitle = prompt("What he bought");
+// var quantity = prompt("How much khareeda");
+// document.write(visitorsName + " ordered " + quantity + " " + productTitle + " from our shop ");
+
+// Chapter4
+
+// Question1
+
+// var a = "blah",
+//     b = "blah blah",
+//     c = "blah blah blah"
+
+// Question2
+
+// var legalVariable;
+// var legalVariable2;
+// var legal3Variable;
+// var $legalvariable4;
+// var _legalvariable5;
+
+// var "illegalvariable";
+// var 2illegalvariable;
+// var function;               no keyword useallow for variable name
+// var illegal variable;
+// var !llegal variable;
+
+// Question 3 (a)
+
+// document.write("<h1>Rules for naming JS variables<h1/>")
+
+// Question 3 (b)
+
+// Numbers, alphabets, Dollar Sign, Underscores
+
+// Question 3 (c)
+
+// dollar, underscores, alphabets
+
+// Question 3 (d)
+
+// Case Sensitive
+
+// Question 3(e)
+
+// JS KEYWORDS
+
+// Chapter 5
+
+// Question 1
+
+// var a = +prompt("Input first Value");
+// var b = +prompt("Input second Value");
+
+// var c = a + b;
+// document.write("Sum of " + a + " and " + b + " is " + c);
+
+// Question 2(a)
+
+// var a = prompt("Input first Value");
+// var b = prompt("Input second Value");
+
+// var c = a - b;
+// document.write("Subtraction of " + a + " and " + b + " is " + c);
+
+// Question 2(b)
+
+// var a = prompt("Input first Value");
+// var b = prompt("Input second Value");
+
+// var c = a * b;
+// document.write("Multiplication of " + a + " and " + b + " is " + c);
+
+// Question 2(c)
+
+// var a = prompt("Input first Value");
+// var b = prompt("Input second Value");
+
+// var c = a / b;
+// document.write("Division of " + a + " and " + b + " is " + c);
+
+// Question 2(d)
+
+// var a = prompt("Input first Value");
+// var b = prompt("Input second Value");
+
+// var c = a % b;
+// document.write("Modulus of " + a + " and " + b + " is " + c);
+
+// Question 3 (a)
+
+// var a;
+
+// Question 3(b)
+
+// document.write("Value after variable declaration is: " + a);
+
+// Question 3(c)
+
+// a = 5;
+
+// Question 3 (d)
+
+// document.write("Initial value of the variable is " + a)
+
+// Question 3 (e)
+
+// ++a;
+
+// Question 3(f)
+
+// document.write("Value after incerement is " + a)
+
+// Question 3(g)
+
+// var b = a + 7;
+
+// Question 3(h)
+
+// document.write("Value after addistion is " + b)
+
+// Question 3(i)
+
+// --b;
+
+// Question 3(j)
+
+// document.write("Value after decreament is " + b)
+
+// Question 3(k)
+
+// var c = b / 3;
+
+// Question 3(l)
+
+// document.write("The Reminder is " + l)
+
+// Question 4
+
+
+// var ticketPrice = 600;
+// var movieCost = ticketPrice * 5;
+// document.write("Total Cost to buy 5 tickets of a movie is " + movieCost);
+
+
+// Question 5
+
+// var a = +prompt("Which Table you want to publish");
+// for (i=1; i < 11; i++) {
+//     document.write(a + "x" + i + "=" + (a*i) + "<br><br>");
+// }
+
+// Question 7
+
+// var firstItem = +prompt("Price of First Item"); 
+// var secondItem = +prompt("Price of Second Item");
+// var quantityItem1 = +prompt("QUnatity of first Item");
+// var quantityItem2 = +prompt("QUnatity of second Item");
+// var shippingCharges = 100;
+
+// document.write("<h1>Shopping Cart</h1> <br> Price of Item1 is " + firstItem + "<br> Quantity of Item1 is " + quantityItem1 + "<br> Price of Item2 is " + secondItem + "<br> Quantity of Item2 is " + quantityItem2 + "<br> Shipping Charges are " + shippingCharges + "<br> Total Cost of your Order is " + ((firstItem*quantityItem1) + (secondItem*quantityItem2)) )
+
+// Question 8
+
+// var totalMarks = +prompt("Total Marks of Exam")
+// var marksObtained = +prompt("Marks obtained by you in exam")
+
+// document.write("<h1>Marks Sheet</h1><br> Total Marks:" + totalMarks + "<br>Marks Obtained:" + marksObtained + "<br>Percentage:" + ((marksObtained/totalMarks)*100))
+
+
+// Question 9
+
+// var saudiRiyals = 25
+// var dollars = 10
+
+// document.write("Total Currency in PKR:" + ((saudiRiyals*28)+(dollars*104.80)))
+
+
+// Question 10
+
+// var a = 3;
+// var singleExpression = ((a+5)*10)/2;
+// document.write(singleExpression)
+
+
+// Question 11
+
+// var currentYear = 2021;
+// var birthYear = 1991;
+
+// document.write("Current Year:" + currentYear + "<br>Birth Year:" + birthYear + "<br>Your Age is:" + (currentYear-birthYear));
+
+// Question 13
+
+// var mySnack = "chocolate chip";
+// var age = 15;
+// var maximumAge = 65;
+// var perDaySnack = 3;
+
+// document.write("You will need " + ((maximumAge-age)*365) + " chocolate chip to last you until the ripe age of " + maximumAge)
+
+
+// Chapter 12-13
+
+// Question 1
+
+// var a = 10;
+
+// if(typeof(a) == "number") {
+//     document.write("its a number");
+// } else {
+//     document.write("its not a number");
+// }
+
+// QUestion 2
+
+// var a = 4;
+// var b = 3;
+
+// if(a<b) {
+//     document.write(b)
+// } else {
+//     document.write(a)
+// }
+
+// Question 3
+
+// var a = -2;
+
+// if(a>0) {
+//     document.write("The Number is Positive");
+// } else {
+//     document.write("The number is negative")
+// }
+
+// Question 4
+
+// var a = "o";
+
+// if(a.length == 1) {
+//     document.write("True")
+// } else {
+//     document.write("False")
+// }
+
+// Question 5
+
+var password = "saylani123"
+var userPassword = prompt("Enter your Password to login")
+
+if(password==userPassword) {
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
